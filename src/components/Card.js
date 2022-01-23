000d8bc201cec624c575f874cc2606ab8f2d86a4
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-01-23 01:00:40 +0100

    cards

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import star from "../img/star.svg";
+
+function Card({img, rating, reviewCount, country, title, price}) {
+  return (
+    <div className="card">
+      <img className="card-image" src={`../img/${img}`} alt="img" />
+      <div className="stars">
+        <img className="star" src={star} alt="star" />
+        <p>
+          {rating} <span className="span-mute">({reviewCount}) {country}</span>{" "}
+        </p>
+      </div>
+      <p className="card-title">{title}</p>
+      <p><span className="span-bold">From ${price}</span> / person</p>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,23 @@
 import Hero from "./Hero";
+import Card from "./Card";
+import data from "../data";
 
 function Main() {
+  const cards = data.map(({ id, coverImg, stats, country, title, price }) => (
+    <Card
+      key={id}
+      img={coverImg}
+      rating={stats.rating}
+      reviewCount={6}
+      country={country}
+      title={title}
+      price={price}
+    />
+  ));
   return (
     <main>
       <Hero />
+      <section className="cards-list">{cards}</section>
     </main>
   );
 }
